4e42b905ab79d2aeeca0d468b4f4572eb1045462
Stephanie Nwankwo <stephybelle208@gmail.com>
2024-05-12 17:50:58 +0100

    Added Files

--- Blog/app.js
+++ Blog/app.js
@@ -9,19 +9,24 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const blogs = [
+    // {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"},
+    // {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"},
+    // {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"}
+  ]
+  res.render('index', { title: "Home", blogs });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.render("about", { title: "About" });
 });
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create')
+  res.render("create", { title: "Create a new blog" });
 })
 
 app.use((req, res) => {
-  res.status(404).render('404');
+  res.status(404).render("404", { title: "404" });
 });
 
 
