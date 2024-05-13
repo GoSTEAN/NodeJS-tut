ef1642c35226f26907c3f99b28e295c5eac54dff
Stephanie Nwankwo <stephybelle208@gmail.com>
2024-05-13 22:18:52 +0100

    Added DB

--- Blog/app.js
+++ Blog/app.js
@@ -1,12 +1,45 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const dbURL = "mongodb+srv://techhunter:pass1234@bloggy.4gv4jur.mongodb.net/Bloggy?retryWrites=true&w=majority";
+
+
+mongoose
+  .connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // ssl: false, // Disable SSL
+    // tls: false, // Disable TLS
+  }).then((result) => console.log('Connected..'))
+
+
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // listen for request
 app.listen(3000);
+
+// middleware and static files
+
+// app.use((req, res, next) => {
+//   console.log('New request made: ');
+//   console.log('Host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   next(); 
+// })
+
+// app.use((req, res, next) => {
+//   console.log("In the next middleware");
+//   next();
+// })
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   const blogs = [
