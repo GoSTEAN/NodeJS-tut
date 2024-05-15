fd994dac2026bf2dcdf18c2b99e7b3e47efc5185
Stephanie Nwankwo <stephybelle208@gmail.com>
2024-05-15 09:24:35 +0100

    Added DB and a new schema

--- Blog/app.js
+++ Blog/app.js
@@ -1,24 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const express = require("express");
+const morgan = require("morgan");
+const { connectDB } = require("./db/mongod");
+const Blog = require('./models/blog')
 
 const app = express();
 
-const dbURL = "mongodb+srv://techhunter:pass1234@bloggy.4gv4jur.mongodb.net/Bloggy?retryWrites=true&w=majority";
-
-
-mongoose
-  .connect(dbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // ssl: false, // Disable SSL
-    // tls: false, // Disable TLS
-  }).then((result) => console.log('Connected..'))
-
-
+connectDB();
 
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // listen for request
 app.listen(3000);
@@ -30,7 +20,7 @@ app.listen(3000);
 //   console.log('Host: ', req.hostname);
 //   console.log('path: ', req.path);
 //   console.log('method: ', req.method);
-//   next(); 
+//   next();
 // })
 
 // app.use((req, res, next) => {
@@ -38,28 +28,45 @@ app.listen(3000);
 //   next();
 // })
 
-app.use(express.static('public'));
-app.use(morgan('dev'));
+app.use(express.static("public"));
+//  morgon for middleware
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => { 
+  const blog = new Blog({
+    title: "new blog",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog.save()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err);
+      
+    })
+})
+
+app.get("/", (req, res) => {
   const blogs = [
     // {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"},
     // {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"},
     // {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"}
-  ]
-  res.render('index', { title: "Home", blogs });
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
-app.get('/about', (req, res) => {
+app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.get('/blogs/create', (req, res) => {
+app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
-})
+});
 
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
-
-
