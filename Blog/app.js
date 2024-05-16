d98caff75de110db6b3c40398669996bc8d970e6
Stephanie Nwankwo <stephybelle208@gmail.com>
2024-05-16 17:16:15 +0100

    Added DB and a new schema

--- Blog/app.js
+++ Blog/app.js
@@ -32,36 +32,72 @@ app.use(express.static("public"));
 //  morgon for middleware
 app.use(morgan("dev"));
 
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => { 
-  const blog = new Blog({
-    title: "new blog",
-    snippet: "about my new blog",
-    body: "more about my new blog",
-  });
+// // mongoose and mongo sandbox routes
+// app.get('/add-blog', (req, res) => { 
+//   const blog = new Blog({
+//     title: "new blog 2",
+//     snippet: "about my new blog",
+//     body: "more about my new blog",
+//   });
 
-  blog.save()
-    .then((result) => {
-      res.send(result)
-    })
-    .catch((err) => {
-      console.log(err);
+//   blog.save()
+//     .then((result) => {
+//       res.send(result)
+//     })
+//     .catch((err) => {
+//       console.log(err);
       
-    })
-})
+//     })
+// })
+
+// app.get('/all-blogs  ', (req, res) => {
+//   Blog.find()
+//     .then((result ) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+      
+//     })
+// })
+
+// app.get('/single-blog', (req, res) => {
+//   Blog.findById("664471d58ee2c31e628384ab")
+//     .then((result) => {
+//       res.send(result)
+//     })
+//     .catch((err) => {
+//       console.log(err);
+      
+//     })
+// })
 
 app.get("/", (req, res) => {
-  const blogs = [
-    // {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"},
-    // {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"},
-    // {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"}
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect('/blogs')
+
+  // Practies
+  // const blogs = [
+  //   {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"},
+  //   {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"},
+  //   {title: "School event tommorrow", snippet: "HOD giving volunteer free sovernier"}
+  // ];
+  // res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find()
+  .then((result) => {
+    res.render('index', { title: 'All Blogs', blogs: result })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+})
 
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
